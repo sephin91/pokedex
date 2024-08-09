8a7d001e71e26c48f52209f6a7c706a8733c27da
조성민 <seongmin.cho@musinsa.com>
2024-08-09 13:15:43 +0900

    loadMore 시점 변경

--- data/src/main/java/com/seongmin/pokedex/data/model/Const.kt
+++ data/src/main/java/com/seongmin/pokedex/data/model/Const.kt
@@ -3,6 +3,6 @@ package com.seongmin.pokedex.data.model
 internal class Const {
     companion object {
         const val MAX_COUNT = 20
-        const val PREFETCH_DISTANCE = 2
+        const val PREFETCH_DISTANCE = 10
     }
 }
